3b0225e7cdad779ea969774636999f5b45f27fd3
progWeb <ramzil.khalilov@mail.ru>
2023-05-06 15:11:01 +0300

    Добавьте файлы проекта.

---
+++ PharmacyCompany/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using PharmacyCompany.Model;
+
+namespace PharmacyCompany
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static PharmacyCompEntities1 DB = new PharmacyCompEntities1();
+    }
+}
